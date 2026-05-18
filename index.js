e1926c48394c5c543007851946731e81e6858ecd
faiaogamamea-star <faiaogamamea@gmail.com>
2026-05-18 00:34:04 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MAMEA from './MAMEA_App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MAMEA />
+  </React.StrictMode>
+);
