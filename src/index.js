054c55b1fa29aca9813255e36eed6df07beeee76
faiaogamamea-star <faiaogamamea@gmail.com>
2026-05-24 18:16:38 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import MAMEA from './MAMEA_App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MAMEA />);
